e19511c8e24d14cae04bf2e1c45449681010bcfb
astianmuchui <astianmuchui@gmail.com>
2024-03-05 00:36:04 +0300

    Fixed bug and shortened the solution

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (let idx of array) {
-    idx += appendString;
+    let value = idx;
+    idx = appendString + value;
   }
 
   return array;
