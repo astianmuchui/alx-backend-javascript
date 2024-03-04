2d7dcffd564cb19ae2afa48f3c4cb7603d6595f9
astianmuchui <astianmuchui@gmail.com>
2024-03-04 22:15:35 +0300

    fixed bug

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,15 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+  const task = false;
+  const task2 = true;
 
-    if (trueOrFalse) {
-        const task = true;
-        const task2 = false;
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
 
-        if (task == true && task2 == true) {
-            return [task, task2];
-          }
-    }
-
-    return [task, task2];
+  return [task, task2];
 }
