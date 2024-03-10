7a782c9be2e3edd5cb8526f05249352ebeddbc4c
astianmuchui <astianmuchui@gmail.com>
2024-03-11 01:13:49 +0300

    allSettled method

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo])
+    .then((results) => {
+      for (const result of results) {
+        if (result.status === 'rejected') {
+          result.value = `Error: ${result.reason.message}`;
+          delete result.reason;
+        }
+      }
+      return results;
+    });
+}
