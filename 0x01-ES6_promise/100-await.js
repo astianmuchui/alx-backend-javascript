f4c83580c703e8bf8f939cb0f1700ac5f54a1a2b
astianmuchui <astianmuchui@gmail.com>
2024-03-11 02:26:57 +0300

    async await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,8 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const user = await createUser();
-  const photo = await uploadPhoto();
-
-  return { photo, user };
+  try {
+    const user = await createUser();
+    const photo = await uploadPhoto();
+    return { user, photo };
+  } catch (err) {
+    return { user: null, photo: null };
+  }
 }
