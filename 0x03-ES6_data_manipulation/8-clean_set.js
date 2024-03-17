1620b79fea0491ce8df455558c4ac01ddd553df7
astianmuchui <astianmuchui@gmail.com>
2024-03-17 15:47:13 +0300

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,5 +2,11 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') {
     return '';
   }
-  return [...set].filter((str) => str.startsWith(startString)).map((str) => str.slice(startString.length)).join('-');
+  const newSet = [];
+  set.forEach((value) => {
+    if (value && value.startsWith(startString)) {
+      newSet.push(value.slice(startString.length));
+    }
+  });
+  return newSet.join('-');
 }
