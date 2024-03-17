ba7f1476ee56801ae44fc081c145238ea2fb49a6
astianmuchui <astianmuchui@gmail.com>
2024-03-17 10:34:24 +0300

    Implement holberto courses class

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,38 @@
 export default class HolbertonCourse {
   constructor(_name, _length, _students) {
-    if (typeof _name === 'string') {
-      this._name = _name;
-    }
-    if (typeof _length === 'number') {
-      this._length = _length;
-    }
-    if (Array.isArray(_students) && _students.every((student) => typeof student === 'string')) {
-      this._students = _students;
-    }
+    this.name = _name;
+    this.length = _length;
+    this.students = _students;
   }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        if (typeof value === 'string') {
+            this._name = value;
+        }
+    }
+
+    get length() {
+        return this._length;
+    }
+
+    set length(value) {
+        if (typeof value === 'number') {
+            this._length = value;
+        }
+    }
+
+    get students() {
+        return this._students;
+    }
+
+    set students(value) {
+        if (Array.isArray(value) && value.every((student) => typeof student === 'string')) {
+            this._students = value;
+        }
+    }
+
 }
