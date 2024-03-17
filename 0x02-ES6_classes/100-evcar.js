d3f51378c00ba8e59613b61350a4f0278abdcc27
astianmuchui <astianmuchui@gmail.com>
2024-03-17 14:22:22 +0300

    EvCar class

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,27 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    const clonedCar = new this.constructor();
+
+    for (const key in this) {
+      if (Object.prototype.hasOwnProperty.call(this, key) && key.startsWith('_')) {
+        clonedCar[key] = this[key];
+      }
+    }
+    return clonedCar;
+  }
+}
