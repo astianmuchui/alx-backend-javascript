a2d7b3ae4197a2a8a4a35e29903c545f2eb35057
astianmuchui <astianmuchui@gmail.com>
2024-03-17 15:01:38 +0300

    Returns an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'Paris' },
+    { id: 2, firstName: 'John', location: 'London' },
+    { id: 3, firstName: 'Marta', location: 'Berlin' },
+  ];
+  return students;
+}
