a7ffdebe50d737fa0da50ba6589c8f12590cf9e2
Sebastian <astianmuchui@gmail.com>
2024-05-01 02:30:33 +0300

    Serves students

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+    res.send(`This is the list of our students\n${await countStudents(process.argv[2])}`);
+    res.end();
+});
+
+const PORT = 1245;
+app.listen(PORT);
+
+module.exports = app;
