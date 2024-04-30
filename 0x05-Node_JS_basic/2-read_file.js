17b4cd777a1a865c3c0eb14f02d2cd90587d7cad
Sebastian <astianmuchui@gmail.com>
2024-05-01 00:29:07 +0300

    Fixed output message

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -38,7 +38,7 @@ function countStudents(path) {
       console.log(`Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(', ')}`);
     });
   } catch (error) {
-    console.error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
 }
 
