f181e0a7bcc7b4df97f275322260e59237f65b6a
Sebastian <astianmuchui@gmail.com>
2024-05-01 01:36:23 +0300

    Fixed Basic http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello Holberton School!\n');
+  res.write('Hello Holberton School!');
   res.end();
 });
 
